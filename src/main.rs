f2c941b5645a262b576085ff8817dbfc3ea31438
Morover <103132521+morover@users.noreply.github.com>
2022-04-06 18:04:56 +0200

    working version

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+use std::path::PathBuf;
+use structopt::StructOpt;
+
+mod client;
+mod engine;
+mod tx;
+
+#[derive(Debug, StructOpt)]
+/// Parses CSV input file with txs, processes them and outputs the state of clients as CSV
+struct Opt {
+    #[structopt(parse(from_os_str))]
+    input_csv: PathBuf,
+}
+
+fn main() -> anyhow::Result<()> {
+    engine::Engine::default().run(Opt::from_args().input_csv)
 }
---
+++ src/tx.rs
@@ -0,0 +1,48 @@
+use anyhow::bail;
+use bigdecimal::BigDecimal;
+use serde::{Deserialize, Serialize};
+use std::collections::HashMap;
+
+use crate::client::Client;
+
+#[derive(Clone, Debug, Serialize, Deserialize)]
+#[serde(tag = "type", rename_all = "snake_case")]
+pub(crate) enum TxType {
+    Deposit { amount: BigDecimal },
+    Withdrawal { amount: BigDecimal },
+    Dispute,
+    Resolve,
+    Chargeback,
+}
+
+#[derive(Clone, Debug, Serialize, Deserialize)]
+pub(crate) struct Tx {
+    #[serde(flatten)]
+    pub(crate) tx_type: TxType,
+    #[serde(rename = "client")]
+    pub(crate) client_id: u16,
+    #[serde(rename = "tx")]
+    pub(crate) tx_id: u32,
+}
+
+impl Tx {
+    pub(crate) fn process(self, clients: &mut HashMap<u16, Client>) -> anyhow::Result<()> {
+        let client = match self.tx_type {
+            TxType::Deposit { .. } => clients
+                .entry(self.client_id)
+                .or_insert(Client::create(self.client_id)),
+            _ => match clients.get_mut(&self.client_id) {
+                None => bail!("Account {} not found", self.client_id),
+                Some(client) => client,
+            },
+        };
+
+        match self.tx_type {
+            TxType::Deposit { amount } => client.deposit(self.tx_id, amount),
+            TxType::Withdrawal { amount } => client.withdraw(amount),
+            TxType::Dispute => client.dispute(&self.tx_id),
+            TxType::Resolve => client.resolve(&self.tx_id),
+            TxType::Chargeback => client.chargeback(&self.tx_id),
+        }
+    }
+}
